46568cf7eeb51b246a6a8fd22daa8c29ba247771
Enyel Sequeira <enyelsequeira@hotmail.com>
2021-05-13 13:10:18 +0800

    link working for outer

--- src/components/MarkdownComponents/MarkdownComponents.tsx
+++ src/components/MarkdownComponents/MarkdownComponents.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
 
-
+const isOuterLink = (link: string) => {
+  if (link === "code" || link === "site") {
+    return "_blank"
+  }
+  return;
+}
 
 const MarkdownComponents = {
   h1: (props) => <p className="text-t-primary text-4xl capitalize font-display" {...props} />,
   h2: (props) => <h2 {...props} />,
   a: (props) => {
-    return <a className="text-red-400 no-underline" target="_blank" href={props.href}>{props.children}</a>
+    // console.log("children", props)
+    return <a className="text-red-400 no-underline" target={isOuterLink(props.children)} href={props.href}>{props.children}</a>
   },
   span: (props) => {
     return <span className="text-t-ternary ">{props.children}</span>
@@ -17,8 +23,8 @@ const MarkdownComponents = {
     console.log(props);
     return (
       <>
-        {props.src && <Image className="rounded-xl" width="500" height="300" src={props.src} alt={props.alt} layout="responsive"  />}
-        </>
+        {props.src && <Image className="rounded-xl" width="500" height="300" src={props.src} alt={props.alt} layout="responsive" />}
+      </>
     )
   },
 
